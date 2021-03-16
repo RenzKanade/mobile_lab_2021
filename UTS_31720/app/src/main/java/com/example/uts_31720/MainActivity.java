14b3ef34a5e23b53abc0f6194c9cf18e8eb1edac
RenzKanade <61531897+RenzKanade@users.noreply.github.com>
2021-03-16 22:20:53 +0700

    Update UTS

--- UTS_31720/app/src/main/java/com/example/uts_31720/MainActivity.java
+++ UTS_31720/app/src/main/java/com/example/uts_31720/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.uts_31720;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView appLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        appLogo = findViewById(R.id.AppLogo);
+        appLogo.setBackgroundResource(R.drawable.applogo);
     }
 }
